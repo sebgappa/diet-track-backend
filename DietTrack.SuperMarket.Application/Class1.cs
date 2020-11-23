ba9e6cbb81c320c7091e15e4e9815d497a9198c1
UKGC <seb.gappa@gmail.com>
2020-11-23 17:44:57 +0000

    Porject setup

---
+++ DietTrack.SuperMarket.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DietTrack.SuperMarket.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ DietTrack.SuperMarket.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DietTrack.SuperMarket.Core
+{
+    public class Class1
+    {
+    }
+}
